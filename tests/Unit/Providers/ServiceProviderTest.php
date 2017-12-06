b1d3fc7280a4c8168a4eb1bb4e88617e993ba35d
Matthew Daly <matthewbdaly@gmail.com>
2017-12-06 21:01:34 +0000

    Added failing test for the comment service provider

---
+++ tests/Unit/Providers/ServiceProviderTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Providers;
+
+use Mockery as m;
+use Tests\TestCase;
+
+class ServiceProviderTest extends TestCase
+{
+    /** @test */
+    public function it_sets_up_the_repository()
+    {
+        $repo = $this->app->make('Matthewbdaly\LaravelComments\Contracts\Comment');
+        $this->assertInstanceOf(\Matthewbdaly\LaravelComments\Eloquent\Repositories\Decorators\Comment::class, $repo);
+    }
+}
