d9b954543917642520fcbebb1b36d3015adb5cfd
Matthew Daly <matthewbdaly@gmail.com>
2017-12-29 19:15:26 +0000

    Updated view

--- src/views/comments.blade.php
+++ src/views/comments.blade.php
@@ -1,4 +1,12 @@
-                        @foreach ($parent->comments as $comment)
-                            {{ $comment->comment }}
-                        @endforeach
-
+<form method="POST" action="/comments">
+    {{ csrf_field() }}
+    <input type="email" name="email" placeholder="Email address" />
+    <input type="url" name="url" placeholder="Website" />
+    <textarea name="comment" placeholder="Enter your comment">
+    </textarea>
+    <input type="hidden" name="commentable_id" value="{{ $parent->id }}" />
+    <input type="hidden" name="commentable_type" value="{{ get_class($parent) }}" />
+</form>
+@foreach ($parent->comments as $comment)
+    {{ $comment->comment }}
+@endforeach
