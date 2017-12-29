4a595459454c089ee32cda527b69220493742cf4
Matthew Daly <matthewbdaly@gmail.com>
2017-12-29 22:13:14 +0000

    Ran CodeSniffer

--- src/Eloquent/Scopes/CommentScope.php
+++ src/Eloquent/Scopes/CommentScope.php
@@ -6,18 +6,21 @@ use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 
+/**
+ * Scope the comments to exclude private and removed ones
+ */
 class CommentScope implements Scope
 {
     /**
      * Apply the scope to a given Eloquent query builder.
      *
-     * @param  \Illuminate\Database\Eloquent\Builder  $builder
-     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  \Illuminate\Database\Eloquent\Builder $builder The query builder instance.
+     * @param  \Illuminate\Database\Eloquent\Model   $model   The model instance.
      * @return void
      */
     public function apply(Builder $builder, Model $model)
     {
         $builder->where('is_public', true)
-        	->where('is_removed', false);
+            ->where('is_removed', false);
     }
 }
