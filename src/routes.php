0af95d0f477068c40eaf5de84b4653e962c34c86
Matthew Daly <matthewbdaly@gmail.com>
2017-12-29 21:38:16 +0000

    Added route for flagging comments

--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,4 @@
 <?php
 
 Route::post('/comments/submit', 'Matthewbdaly\LaravelComments\Http\Controllers\CommentController@store')->name('comments.submit');
+Route::post('/comments/flag', 'Matthewbdaly\LaravelComments\Http\Controllers\CommentController@flag')->name('comments.flag');
